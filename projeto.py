f47b51ce9da3fd294ae0086f29076397e4053da0
unknown <gabrielhenryque311@gmail.com>
2023-02-20 18:39:07 -0300

    madara

--- projeto.py
+++ projeto.py
@@ -1,5 +1,16 @@
 import requests as rq
-print(rq)
+lista_de_pokemon=["pikachu","charizard","squirtle","Bulbasaur"]
+
 BASE_URL="https://pokeapi.co/api/v2/"
-x=rq.get(BASE_URL + "pokemon/" + "pikachu")
-print(x.text,"x")
+
+for x in lista_de_pokemon:
+    pokemon={"nome":"","nome":"","nome":"","nome":"",}
+    print(x)
+
+    k=rq.get(BASE_URL + "pokemon/" + x )
+
+    resposta=k.json()
+    resposta["abilities"]
+    print(resposta["abilities"])
+#for y in resposta:
+ # print(y)
